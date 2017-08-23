d64be89df480eaa8f691d2c8dc4d25b25a6da2b0
Jannis R <mail@jannisr.de>
2017-08-23 19:25:18 +0200

    quit, delete

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const level = require('level')
 
 const createSlice = require('./lib/slice')
 
+const noop = () => {}
+
 const UI = {
 	moveCursor: function (n) {
 		this.cursor = n
@@ -25,15 +27,13 @@ const UI = {
 	},
 	abort: function () {
 		this.done = this.aborted = true
-		this.render()
-		this.out.write('\n')
+		this.out.write(esc.clearScreen)
 		this.close()
 	},
 	submit: function () {
 		this.done = true
 		this.aborted = false
-		this.render()
-		this.out.write('\n')
+		this.out.write(esc.clearScreen)
 		this.close()
 	},
 
@@ -57,7 +57,52 @@ const UI = {
 		this.render()
 	},
 
-	_: function (c) { // todo
+	delete: function (cb = noop) {
+		const e = this.entries[this.cursor]
+		if (!e) return this.bell()
+
+		e.pending = true
+		this.render()
+
+		const self = this
+		this.db.del(e.rawKey, (err) => {
+			if (err) {
+				self.feedback(err, true)
+				setImmediate(cb, err)
+				return
+			}
+
+			const i = self.entries.indexOf(e)
+			self.entries = self.entries.slice() // clone
+			self.entries.splice(i, 1) // delete
+
+			self.feedback(e.key + ' deleted.', false)
+			setImmediate(cb)
+		})
+	},
+
+	_: function (c) {
+		if (c === 'd') this.delete()
+		else if (c === 'q') this.submit() // quit
+		// todo
+	},
+
+	message: null,
+	msgIsError: false,
+	msgTimeout: null,
+	feedback: function (msg, isError) {
+		const self = this
+		if (this.msgTimeout !== null) clearTimeout(this.msgTimeout)
+		this.msgTimeout = setTimeout(() => {
+			self.message = null
+			self.msgIsError = false
+			self.msgTimeout = null
+			self.render()
+		}, 2 * 1000)
+
+		this.message = splitLines(msg.message || msg + '')[0]
+		this.msgIsError = isError
+		this.render()
 	},
 
 	clear: '',
@@ -74,16 +119,19 @@ const UI = {
 
 				out += [
 					i === this.cursor ? chalk.blue(e.key) : e.key,
-					chalk.gray(e.value)
+					chalk.gray(e.value),
+					i
 				].join(' ') + '\n'
 			}
 		} else out += chalk.red('no entries') + '\n'
 
-		if (this.error) {
-			const msg = this.error.message || splitLines(this.error + '')[0]
-			out += chalk.bgRed.black(msg) + '\n'
+		const msg = this.message
+		if (msg !== null) {
+			out += this.msgIsError
+				? chalk.bgRed.black(msg)
+				: chalk.bgGreen.black(msg)
 		} else {
-			out += chalk.bgGreen.black(this.dbName) + '\n'
+			out += chalk.bgGreen.black(this.dbName)
 		}
 
 		this.out.write(this.clear + out)
@@ -93,7 +141,6 @@ const UI = {
 
 const defaults = {
 	value: null,
-	error: null,
 	done: false,
 	aborted: false
 }
@@ -116,6 +163,8 @@ const ui = (dbPath) => {
 
 	ui.entries = []
 	ui.cursor = 0
+
+	slice.on('error', (err) => ui.onError(err))
 	slice.on('move', (entries) => {
 		ui.entries = entries
 		if (entries.length > 0) {
@@ -124,18 +173,6 @@ const ui = (dbPath) => {
 		ui.render()
 	})
 	setImmediate(slice.move, 'down', 100)
-
-	let errorTimeout = null
-	slice.on('error', (err) => {
-		ui.error = err
-		if (errorTimeout !== null) clearTimeout(errorTimeout)
-		errorTimeout = setTimeout(clearError)
-		ui.render()
-	})
-	const clearError = () => {
-		ui.error = null
-		ui.render()
-	}
 
 	return wrap(ui)
 }
